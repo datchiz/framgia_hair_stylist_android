d723c9caa7b1f24bbfadfd74845b28f8e7c9a205
ToanDoan <toandoan.coder@gmail.c>
2017-07-28 08:57:08 +0700

    add fabric

--- app/src/main/java/com/framgia/fsalon/FSalonApplication.java
+++ app/src/main/java/com/framgia/fsalon/FSalonApplication.java
@@ -1,18 +1,22 @@
 package com.framgia.fsalon;
 
 import android.app.Application;
-
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
 import com.framgia.fsalon.data.source.api.FSalonServiceClient;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by MyPC on 20/07/2017.
  */
 public class FSalonApplication extends Application {
+
     private static FSalonApplication sInstant;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         FSalonServiceClient.initialize(this);
         sInstant = this;
     }
